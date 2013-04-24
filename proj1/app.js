415b6b5ffdfb0a5b000a4228e50438e1a843e9b1
Suvi Vignarajah <suvethan@gmail.com>
2013-04-24 00:28:12 -0400

    adding first attempt at curl requests with node - with included node modules

---
+++ proj1/app.js
@@ -0,0 +1,37 @@
+var express = require("express")
+	, curl = require("curlrequest")
+	, app = express()
+	;
+
+app.get('/curlRequest', function (req, res) {
+	
+	var reqURL = ''
+		, options = { url: reqURL, include: true }
+		;
+
+	curl.request(options, function (err, parts) {
+	    	parts = parts.split('\r\n');
+	    	var data = parts.pop()
+	      		, head = parts.pop()
+	      		;
+	      	
+	      	res.header('Content-Type', 'application/json');
+  		res.header('Charset', 'utf-8')
+	      	res.send( 'resData(' + data + ');' );
+
+	      	/*
+		*Sample Request would look like:
+		* $.ajax({
+               *        dataType: 'jsonp',                    
+               *        jsonpCallback: 'resData',
+               *        url: '/curlRequest',                    
+               *        success: function(data) {
+               *            console.log(data);               
+               *        }
+               * });
+	      	*/
+	});
+
+});
+
+app.listen(3000); 
